126b0039be9ec86af1462c79eb9f59dc6eb42324
Jorge Zapata <jorge.zapatas@est.iudigital.edu.co>
2026-06-07 21:05:07 -0500

    act

---
+++ src/ArbolInventario.java
@@ -0,0 +1,29 @@
+public class ArbolInventario {
+    private Producto raiz;
+
+    public ArbolInventario() {
+        this.raiz = null;
+    }
+
+    public void insertar(int id, String nombre){
+        raiz = insertarRecursivo(raiz, id, nombre);
+    }
+
+    private Producto insertarRecursivo(Producto nodoActual, int id, String nombre){
+        if (nodoActual == null) {
+            return new Producto(id, nombre);
+        }
+
+        if (id < nodoActual.id) {
+            nodoActual.izquierdo = insertarRecursivo(nodoActual.izquierdo, id, nombre);
+        } else if (id > nodoActual.id) {
+            nodoActual.derecho = insertarRecursivo(nodoActual.derecho, id, nombre);
+        } else {
+            System.out.println("Ya existe un producto con ID" + id + ". No se insertó");
+        }
+        return nodoActual;
+    }
+    
+    public void mostrarInorden() {
+        if ()
+    }
